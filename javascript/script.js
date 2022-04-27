dc26b12f707ce865263ff32442905ee3c263a864
judicomuhitu <104528222+judicomuhitu@users.noreply.github.com>
2022-04-27 23:30:36 +0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,7 @@ const step = 1;
 
 function outNum(num, elem) {
    let l = document.querySelector('#' + elem);
-   n = 3486;
+   n = 247;
    let t = Math.round(time/(num/step));
    let interval = setInterval(() => {
       n = n + step;
@@ -15,4 +15,4 @@ function outNum(num, elem) {
       t);
 }
 
-outNum(7543, 'out-1');
+outNum(555, 'out-1');
